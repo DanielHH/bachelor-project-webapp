4c8ed2737c5a329e99bfe49f3556b4785a7a389c
Niklas Nilsson <nikni459@student.liu.se>
2018-02-01 23:30:59 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { routing } from './routing';
+
+
+import { HomeComponent } from './pages/home/home.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
+import { ReceiptsComponent } from './pages/receipts/receipts.component';
+import { LogsComponent } from './pages/logs/logs.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DocumentsComponent,
+    ReceiptsComponent,
+    LogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
+import { ReceiptsComponent } from './pages/receipts/receipts.component';
+import { LogsComponent } from './pages/logs/logs.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent, data: { name: 'Startsidan' } },
+    { path: 'documents', component: DocumentsComponent, data: { name: 'Handlingar' } },
+    { path: 'receipts', component: ReceiptsComponent, data: { name: 'Kvittenser' } },
+    { path: 'logs', component: LogsComponent, data: { name: 'Loggar' } }
+
+	/* Resterande routes
+	{ path: '**', component: PageNotFoundComponent, data: { name: 'SidanHittadesInte', fullSizeBackgroundURL: '/assets/not-found.jpg' } }
+    */
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: false });
