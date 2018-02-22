e52fbdfb966a008a589a7e849f995775d84d1941
Niklas Nilsson <nikni459@student.liu.se>
2018-02-22 23:31:23 +0100

    Added get card types to dataservice

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { Card } from '../datamodels/card';
 import { HttpService } from './http.service';
+import { CardType } from '../datamodels/cardType';
 
 @Injectable()
 export class DataService {
@@ -19,18 +20,39 @@ export class DataService {
     this._cardList
   );
 
+  /**
+   * List with all cards
+   */
+  _cardTypeList: CardType[] = [];
+
+  /**
+   * A subscriber to the card list
+   */
+  cardTypeList: BehaviorSubject<CardType[]> = new BehaviorSubject<CardType[]>(
+    this._cardTypeList
+  );
+
   constructor(public httpService: HttpService) {
     this.getAllData();
   }
 
   getAllData() {
     this.getCardList();
+    this.getCardTypeList();
   }
 
   getCardList() {
-    this.httpService.httpGet<Card>('getAllCards').then(data => {
+    this.httpService.httpGet<Card>('getCards').then(data => {
       this._cardList = data;
       this.cardList.next(this._cardList);
+      console.log(data);
+    });
+  }
+
+  getCardTypeList() {
+    this.httpService.httpGet<CardType>('getCardTypes').then(data => {
+      this._cardTypeList = data;
+      this.cardTypeList.next(this._cardTypeList);
       console.log(data);
     });
   }
