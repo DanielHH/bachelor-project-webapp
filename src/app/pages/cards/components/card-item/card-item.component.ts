d8d7223a138a919820c239ff54d4ed0cb78d4240
Philip Bengtsson <phibe092@student.liu.se>
2018-02-27 14:20:40 +0100

    Circumvented error of undefined property

--- src/app/pages/cards/components/card-item/card-item.component.ts
+++ src/app/pages/cards/components/card-item/card-item.component.ts
@@ -12,55 +12,59 @@ import * as _ from 'lodash';
   styleUrls: ['./card-item.component.scss']
 })
 export class CardItemComponent implements OnInit {
-
   @Input() cardItem: Card;
 
   cardTypeList: CardType[] = [];
   userList: User[] = [];
 
   constructor(public dataService: DataService) {
-    this.dataService.cardTypeList.subscribe( (cardTypeList) => {
+    this.dataService.cardTypeList.subscribe(cardTypeList => {
       this.cardTypeList = cardTypeList;
     });
-    this.dataService.userList.subscribe( (userList) => {
+    this.dataService.userList.subscribe(userList => {
       this.userList = userList;
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   /**
-  * Submits a checkout
-  */
+   * Submits a checkout
+   */
   submitRequest() {
     this.setStatus(1); // TODO: ENUM/DATATYPE?
   }
 
   /**
-  * Submits a checkin
-  */
+   * Submits a checkin
+   */
   submitReturn() {
     this.setStatus(0); // TODO: ENUM/DATATYPE?
   }
 
   /**
-  * Inverts the card status active/inactive
-  */
-  setStatus(status: Number) { // TODO: ENUM/DATATYPE?
+   * Inverts the card status active/inactive
+   */
+  setStatus(status: Number) {
+    // TODO: ENUM/DATATYPE?
     this.cardItem.status = status;
   }
 
   displayCardType() {
-    if (this.cardItem.cardType) {
-      return _.find(this.cardTypeList, (cardType) => cardType.id === this.cardItem.cardType).name;
+    if (this.cardItem.cardType > 0) {
+      const test = _.find( this.cardTypeList, cardType => cardType.id === this.cardItem.cardType);
+      console.log(test);
+      if (test) {
+        return test.name;
+      }
     }
     return '';
   }
 
   displayUserName() {
     if (this.cardItem.userID) {
-      return _.find(this.userList, (user) => user.id === this.cardItem.userID).name;
+      return _.find(this.userList, user => user.id === this.cardItem.userID)
+        .name;
     }
     return '';
   }
@@ -71,5 +75,4 @@ export class CardItemComponent implements OnInit {
   displayExpirationDate() {
     return moment(this.cardItem.expirationDate).format('YYYY-MM-DD');
   }
-
 }
