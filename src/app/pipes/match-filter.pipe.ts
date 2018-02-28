3299ddeb30d4a48ef081cc55bf1ed32277b7cc9d
David Hasselquist <david@hasselq.se>
2018-02-28 10:24:23 +0100

    Changed filter to filter on actual data and not just id numbers

--- src/app/pipes/match-filter.pipe.ts
+++ src/app/pipes/match-filter.pipe.ts
@@ -2,12 +2,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Card } from '../datamodels/card';
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import { DataService } from '../services/data.service';
+import { User } from '../datamodels/user';
+import { CardType } from '../datamodels/cardType';
 
 @Pipe({
   name: 'matchFilter'
 })
 
 export class MatchFilterPipe implements PipeTransform {
+
+  cardTypeList: CardType[] = [];
+  userList: User[] = [];
+
+  constructor(public dataService: DataService) {
+    this.dataService.cardTypeList.subscribe(cardTypeList => {
+      this.cardTypeList = cardTypeList;
+    });
+    this.dataService.userList.subscribe(userList => {
+      this.userList = userList;
+    });
+  }
+
 
   transform(value: Card[], input: string): Card[] {
     if (input !== '') {
@@ -23,14 +39,34 @@ export class MatchFilterPipe implements PipeTransform {
     const displayDate = moment(card.expirationDate).format('YYYY-MM-DD');
 
     if (_.includes(_.lowerCase(card.cardNumber), filterInput) === false
-    && (_.includes(_.lowerCase(_.toString(card.cardType)), filterInput) === false)
-    && (_.includes(_.lowerCase(_.toString(card.userID)), filterInput) === false)
+    && (_.includes(_.lowerCase(this.getCardType(card)), filterInput) === false)
+    && (_.includes(_.lowerCase(this.getUserName(card)), filterInput) === false)
     && (_.includes(_.lowerCase(card.comment), filterInput) === false)
     && (_.includes(_.lowerCase(card.location), filterInput) === false)
     && (_.includes(_.lowerCase(displayDate), filterInput) === false) ) {
       return false;
     }
     return true;
+  }
+
+  getCardType(card: Card) {
+    if (card.cardType > 0) {
+      const cardTypeToDisplay = _.find( this.cardTypeList, cardType => cardType.id === card.cardType);
+      if (cardTypeToDisplay) {
+        return cardTypeToDisplay.name;
+      }
+    }
+    return '';
+  }
+
+  getUserName(card: Card) {
+    if (card.cardType > 0) {
+      const userToDisplay = _.find( this.userList, user => user.id === card.userID);
+      if (userToDisplay) {
+        return userToDisplay.name;
+      }
+    }
+    return '';
   }
 
 }
