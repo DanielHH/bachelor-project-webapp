76a85a780c8fa342690c562c2a38f8e56708726f
David Hasselquist <david@hasselq.se>
2018-03-22 10:12:33 +0100

    Copied and duplicated document table/item to delivery

--- src/app/pages/deliveries/components/delivery-item/delivery-item.component.ts
+++ src/app/pages/deliveries/components/delivery-item/delivery-item.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DocumentType } from '../../../../datamodels/documentType';
+import { Document } from '../../../../datamodels/document';
+import { User } from '../../../../datamodels/user';
+import { DataService } from '../../../../services/data.service';
+import { RouteDataService } from '../../../../services/route-data.service';
+import { Router } from '@angular/router';
+import { HttpService } from '../../../../services/http.service';
+import _ = require('lodash');
 
 @Component({
   selector: 'app-delivery-item',
@@ -6,10 +14,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delivery-item.component.scss']
 })
 export class DeliveryItemComponent implements OnInit {
+  @Input() documentItem: Document;
+  @Output() editItem = new EventEmitter<any>();
 
-  constructor() { }
+  documentTypeList: DocumentType[] = [];
+  userList: User[] = [];
 
-  ngOnInit() {
+  showRequestModal = false;
+
+  showReturnModal = false;
+
+  constructor(
+    public dataService: DataService,
+    private routeDataService: RouteDataService,
+    private router: Router,
+    private httpService: HttpService
+  ) {
+    this.dataService.documentTypeList.subscribe(documentTypeList => {
+      this.documentTypeList = documentTypeList;
+    });
+    this.dataService.userList.subscribe(userList => {
+      this.userList = userList;
+    });
   }
 
+  ngOnInit() { }
+
+  /**
+   * Change card status
+   */
+  setDocumentStatus(status: number) {
+    this.documentItem.status = status;
+    this.httpService.httpPut<Document>('updateDocument/', this.documentItem).then(res => {
+      if (res.message === 'success') {
+        this.showRequestModal = false;
+        this.showReturnModal = false;
+      }
+    });
+  }
+
+  /**
+   * Returns the name of the document type corresponding to the documentType
+   */
+  displayDocumentType() {
+    if (this.documentItem.documentType > 0) {
+      const documentTypeToDisplay = _.find(this.documentTypeList, documentType => documentType.id === this.documentItem.documentType);
+      if (documentTypeToDisplay) {
+        return documentTypeToDisplay.name;
+      }
+    }
+    return '';
+  }
+
+  /**
+   * Returns the name corresponding to the userID
+   */
+  displayUserName() {
+    if (this.documentItem.userID) {
+      return _.find(this.userList, user => user.id === this.documentItem.userID)
+        .name;
+    }
+    return '';
+  }
+  route() {
+    this.routeDataService.document.next(this.documentItem);
+    this.router.navigate(['document-detail']);
+  }
+
+  /**
+   * Set document to be outputted for editing
+   */
+  edit() {
+    this.editItem.next(this.documentItem);
+  }
+
+  /**
+   * Show modal based on status
+   */
+  showModal() {
+    if (this.documentItem.status === 1) {
+      this.showRequestModal = true;
+    } else {
+      this.showReturnModal = true;
+    }
+  }
 }
--- src/app/pages/deliveries/components/delivery-table/delivery-table.component.ts
+++ src/app/pages/deliveries/components/delivery-table/delivery-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import _ = require('lodash');
 
 @Component({
   selector: 'app-delivery-table',
@@ -6,10 +7,128 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delivery-table.component.scss']
 })
 export class DeliveryTableComponent implements OnInit {
+  
+  @Input() documentList: Document[];
+
+  editDocument: Document = null; // Document to be edited
+
+  showModal = false;
+
+  filterInput = '';
+
+  orderStatus = '';
+  orderDocumentType = '';
+  orderDocumentNumber = '';
+  orderName = '';
+  orderUserID = '';
+  orderLocation = '';
+  orderComment = '';
+
+  showIn = true;
+  showOut = true;
+  showArchived = false;
+  showGone = false;
+  modalTitle = "";
+
+  modalType = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.sortTableListStart();
   }
 
+  /**
+   * Sorts table after modifiedDate ascending
+   */
+  sortTableListStart() {
+    this.documentList = _.orderBy(this.documentList, ['modifiedDate'], ['desc']);
+  }
+
+  /**
+   * Sorts the table depending on the property of the Card
+   * @param property
+   */
+  sortTableList(property: string) {
+    let newOrder = '';
+
+    switch (property) {
+      case 'status': {
+        newOrder = this.sortTableListHelper(this.orderStatus);
+        this.orderStatus = newOrder;
+        break;
+      }
+      case 'documentType': {
+        newOrder = this.sortTableListHelper(this.orderDocumentType);
+        this.orderDocumentType = newOrder;
+        break;
+      }
+      case 'documentNumber': {
+        newOrder = this.sortTableListHelper(this.orderDocumentNumber);
+        this.orderDocumentNumber = newOrder;
+        break;
+      }
+      case 'name': {
+        newOrder = this.sortTableListHelper(this.orderName);
+        this.orderName = newOrder;
+        break;
+      }
+      case 'userID': {
+        newOrder = this.sortTableListHelper(this.orderUserID);
+        this.orderUserID = newOrder;
+        break;
+      }
+      case 'location': {
+        newOrder = this.sortTableListHelper(this.orderLocation);
+        this.orderLocation = newOrder;
+        break;
+      }
+      case 'comment': {
+        newOrder = this.sortTableListHelper(this.orderComment);
+        this.orderComment = newOrder;
+        break;
+      }
+    }
+
+    if (newOrder) {
+      this.documentList = _.orderBy(this.documentList, [property], [newOrder]);
+    }
+
+  }
+
+
+  /**
+   * Sets the order to sort by
+   * @param order
+   */
+  sortTableListHelper(order: string) {
+    switch (order) {
+      case 'asc': return 'desc';
+      default: return 'asc';
+    }
+  }
+
+  /**
+   * Set document to be edited and open edit modal
+   */
+  openEdit(document: any) {
+    this.editDocument = document;
+    this.modalTitle = "Edit document";
+    this.modalType = 1;
+    this.showModal = true;
+  }
+
+  /**
+   * Set document to be edited and open edit modal
+   */
+  openAddNewDocument() {
+    this.editDocument = Object.assign({}, new Document());
+    this.modalTitle = "Add new document";
+    this.modalType = 0;
+    this.showModal = true;
+  }
+
+
+
 }
+
