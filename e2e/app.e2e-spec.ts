9e44fd026db8e05420fdb865895bf7a33c8c7ab4
Andreas <andlu984@student.liu.se>
2018-03-25 13:50:57 +0200

    Tests for basic navigation from side-menu implemented

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,37 @@
 import { AppPage } from './app.po';
-import { browser } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('PUMApp', function() {
-  it('should have a title', function() {
-    browser.get('http://pum.nlsn.se/');
+  const documentsFromMenu = element(by.linkText('Handlingar'));
+  const cardFromMenu = element(by.linkText('Kort'));
+  const receiptsFromMenu = element(by.linkText('Kvittenser'));
+  const logsFromMenu = element(by.linkText('Loggar'));
 
+  beforeEach(function() {
+    browser.get('http://pum.nlsn.se/');
+  });
+
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('PUMApp');
+  });
+
+  it('should work to click documents in the side-menu', function() {
+    documentsFromMenu.click();
+    expect(browser.getCurrentUrl()).toEqual('http://pum.nlsn.se/documents');
+  });
+
+  it('should work to click cards in the side-menu', function() {
+    cardFromMenu.click();
+    expect(browser.getCurrentUrl()).toEqual('http://pum.nlsn.se/cards');
+  });
+
+  it('should work to click receipts in the side-menu', function() {
+    receiptsFromMenu.click();
+    expect(browser.getCurrentUrl()).toEqual('http://pum.nlsn.se/receipts');
+  });
+
+  it('should work to click logs in the side-menu', function() {
+    logsFromMenu.click();
+    expect(browser.getCurrentUrl()).toEqual('http://pum.nlsn.se/logs');
   });
 });
