8bc8932f42d0351fabfa060af795c1e30eb374a3
Andreas <andlu984@student.liu.se>
2018-03-25 21:34:55 +0200

    Added tests for some of the options in dropdown for cards

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,11 +1,16 @@
 import { AppPage } from './app.po';
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions, Button } from 'protractor';
 
 describe('PUMApp', function() {
+
   const documentsFromMenu = element(by.linkText('Handlingar'));
   const cardFromMenu = element(by.linkText('Kort'));
   const receiptsFromMenu = element(by.linkText('Kvittenser'));
   const logsFromMenu = element(by.linkText('Loggar'));
+
+  const editFromDropdown = element(by.linkText('Ändra'));
+  const lostFromDropdown = element(by.linkText('Borttappat'));
+  const resetFromDropdown = element(by.linkText('Återställ'));
 
   beforeEach(function() {
     browser.get('http://pum.nlsn.se/');
@@ -33,5 +38,31 @@ describe('PUMApp', function() {
   it('should work to click logs in the side-menu', function() {
     logsFromMenu.click();
     expect(browser.getCurrentUrl()).toEqual('http://pum.nlsn.se/logs');
+  });
+
+  it('should work to click edit in the dropdown-menu', function() {
+    cardFromMenu.click();
+    element.all(by.className('btn-menu')).first().click();
+    editFromDropdown.click();
+    browser.sleep(500);
+    expect(element(by.buttonText('Submit')).isDisplayed()).toBe(true);
+    element(by.id('locationInput')).clear();
+    element(by.id('locationInput')).sendKeys('Skåp 55');
+    browser.sleep(500);
+    element(by.buttonText('Submit')).click();
+    browser.sleep(500);
+    expect(element.all(by.className('col')).getText()).toContain('Skåp 55');
+  });
+
+  it('should work to click lost and reset in the dropdown-menu', function() {
+    cardFromMenu.click();
+    element.all(by.className('btn-menu')).first().click();
+    lostFromDropdown.click();
+    browser.sleep(500);
+    expect(element(by.buttonText('Borta')).isDisplayed()).toBe(true);
+    element.all(by.className('btn-menu')).first().click();
+    resetFromDropdown.click();
+    browser.sleep(500);
+    expect(element.all(by.buttonText('Kvittera')).first().isDisplayed()).toBe(true);
   });
 });
