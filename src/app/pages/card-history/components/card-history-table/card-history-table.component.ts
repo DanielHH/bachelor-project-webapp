5d5056a24ff760e3039d292e1551dcd1fbca0570
Jennifer Lindgren <jennifer.lindgren93@gmail.com>
2018-05-21 20:31:06 +0200

    Sorting fixed for types, users, card history and document history

--- src/app/pages/card-history/components/card-history-table/card-history-table.component.ts
+++ src/app/pages/card-history/components/card-history-table/card-history-table.component.ts
@@ -1,18 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import * as _ from 'lodash';
 import { Card } from '../../../../datamodels/card';
 import { LogEvent } from '../../../../datamodels/logEvent';
 import { lowerCase } from '../../../../services/utilities.service';
 import { ModalService } from '../../../../services/modal.service';
+import { RouteDataService } from '../../../../services/route-data.service';
+import { DataService } from '../../../../services/data.service';
 
 @Component({
   selector: 'app-card-history-table',
   templateUrl: './card-history-table.component.html',
   styleUrls: ['./card-history-table.component.scss']
 })
-export class CardHistoryTableComponent implements OnInit {
-  @Input() logEventList: LogEvent[];
-  @Input() card: Card;
+export class CardHistoryTableComponent implements OnInit, OnDestroy {
+  card: Card;
+
+  order = 'desc';
+  sortProperty = 'logDate';
 
   filterInput = '';
 
@@ -24,72 +28,111 @@ export class CardHistoryTableComponent implements OnInit {
   showReceipt = true;
   showOther = true;
 
-  constructor(private modalService: ModalService) {}
+  dataServiceSubscriber: any;
+  routeDataServiceSubscriber: any;
+
+  logEventList: LogEvent[] = [];
+  filteredLogEventList: LogEvent[] = [];
+
+  constructor(
+    private modalService: ModalService,
+    private dataService: DataService,
+    private routeDataService: RouteDataService
+  ) {
+    this.routeDataServiceSubscriber = this.routeDataService.card.subscribe(card => {
+      this.card = card;
+      if (this.logEventList) {
+        this.filteredLogEventList = _.filter(this.logEventList, logEvent => {
+          if (logEvent.card && logEvent.card.id) {
+            return logEvent.card.id == this.card.id;
+          }
+          return false;
+        });
+      }
+      this.orderTableList();
+    });
+    this.dataServiceSubscriber = this.dataService.logEventList.subscribe(logEventList => {
+      this.logEventList = logEventList;
+      if (this.card && this.card.id) {
+        this.filteredLogEventList = _.filter(this.logEventList, logEvent => {
+          if (logEvent.card && logEvent.card.id) {
+            return logEvent.card.id == this.card.id;
+          }
+          return false;
+        });
+      }
+      this.orderTableList();
+    });
+  }
 
   ngOnInit() {
-    this.sortTableListStart();
+    this.orderTableList();
+  }
+
+  ngOnDestroy() {
+    this.dataServiceSubscriber.unsubscribe();
+    this.routeDataServiceSubscriber.unsubscribe();
   }
 
   /**
-   * Sorts table after modifiedDate ascending
-   */
-  sortTableListStart() {
-    this.logEventList = _.orderBy(this.logEventList, ['logDate'], ['desc']);
-  }
-
-  /**
-   * Sorts the table depending on the property of the log
+   * Update order and order property
    * @param property
    */
-  sortTableList(property: string) {
-    let newOrder = '';
+  updateOrder(property: string) {
+    this.sortProperty = property;
 
     switch (property) {
       case 'logDate': {
-        newOrder = this.sortTableListHelper(this.orderLogDate);
-        this.orderLogDate = newOrder;
+        this.order = this.getNewOrder(this.orderLogDate);
+        this.orderLogDate = this.order;
         break;
       }
       case 'logType': {
-        newOrder = this.sortTableListHelper(this.orderLogType);
-        this.orderLogType = newOrder;
+        this.order = this.getNewOrder(this.orderLogType);
+        this.orderLogType = this.order;
         property = 'logType.name';
         break;
       }
       case 'logText': {
-        newOrder = this.sortTableListHelper(this.orderLogText);
-        this.orderLogText = newOrder;
+        this.order = this.getNewOrder(this.orderLogText);
+        this.orderLogText = this.order;
         break;
       }
       case 'user': {
-        newOrder = this.sortTableListHelper(this.orderLogUser);
-        this.orderLogUser = newOrder;
+        this.order = this.getNewOrder(this.orderLogUser);
+        this.orderLogUser = this.order;
         property = 'user.name';
         break;
       }
     }
-    if (newOrder) {
-      this.logEventList = _.orderBy(
-        this.logEventList,
+  }
+
+  /**
+   * Orders card list based on set order and order property
+   */
+  orderTableList() {
+    if (this.order) {
+      this.filteredLogEventList = _.orderBy(
+        this.filteredLogEventList,
         [
           logEvent => {
-            if (logEvent[property]) {
-              return lowerCase(logEvent[property]) as string;
+            if (logEvent[this.sortProperty]) {
+              return lowerCase(logEvent[this.sortProperty]) as string;
             } else {
-              return logEvent[property.slice(0, -5)] ? (lowerCase(logEvent[property.slice(0, -5)].name) as string) : '';
+              return logEvent[this.sortProperty.slice(0, -5)] ? (lowerCase(logEvent[this.sortProperty.slice(0, -5)].name) as string) : '';
             }
           }
         ],
-        [newOrder]
+        [this.order]
       );
     }
   }
 
   /**
-   * Sets the order to sort by
+   * Returns new order given old one
    * @param order
    */
-  sortTableListHelper(order: string) {
+  getNewOrder(order: string) {
     switch (order) {
       case 'asc':
         return 'desc';
